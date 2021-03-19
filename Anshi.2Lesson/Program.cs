c45142b50f740bb8b498239b6dc24ad9368d08d7
Anshi <albacirus@gmail.com>
2021-03-20 00:34:57 +0200

    Код калькулятора без доработок

---
+++ Anshi.2Lesson/Program.cs
@@ -0,0 +1,104 @@
+﻿
+
+using System;
+
+namespace Anshi._2Lesson
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                double FirstValue, SecondValue;
+                string action;
+
+
+                Console.WriteLine("Введите первое число");
+                FirstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число");
+                SecondValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию:'+' '-' '*' '/' ");
+
+                action = Console.ReadLine();
+                switch (action)
+                {
+
+
+                    default:
+                        Console.WriteLine("Ошибка неизвестная операция");
+                        break;
+                    case "+":
+                        Console.WriteLine(FirstValue + SecondValue);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(FirstValue - SecondValue);
+                        break;
+
+                    case "/":
+                        Console.WriteLine(FirstValue / SecondValue);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(FirstValue * SecondValue);
+                        break;
+                    case "q":
+                        return;
+                        
+                        
+                }
+                Console.WriteLine("Нажмите клавишу q для выполнения следуйщей операции");
+                Console.ReadLine();
+                
+                Console.Clear();
+
+            }
+
+
+
+        }
+
+                
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+           
+
+       
+      
+
+
+
+
+
+
+
+              
+            
+            
+            
+
+
+
+
+
+
+
+        
+   
+
